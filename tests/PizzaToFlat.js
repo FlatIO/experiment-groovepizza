6305f9ab22b93975ed2fc3a58a00b2673c386f3a
Vincent Giersch <vincent@giersch.fr>
2017-04-30 20:51:33 +0200

    fix(midi): unpitched id format

--- tests/PizzaToFlat.js
+++ tests/PizzaToFlat.js
@@ -26,8 +26,79 @@ describe('Pizza to Flat', function () {
       assert.equal(chameleon['score-partwise'].credit[0]['credit-words'], 'Chameleon');
     });
 
-    it('should have the 3 instruments par headers', function () {
+    it('should have the 3 instruments part headers', function () {
+      assert.ok(chameleon['score-partwise']['part-list']);
+      assert.equal(chameleon['score-partwise']['part-list']['score-part'].length, 3);
+      var scorePart = chameleon['score-partwise']['part-list']['score-part'];
 
+      assert.deepEqual(scorePart[0], {
+        'part-name': 'Kick',
+        'part-abbreviation': '',
+        'score-instrument': {
+          'instrument-name': 'Kick',
+          '$id': 'P1-X1'
+        },
+        'midi-instrument': {
+          'midi-unpitched': '36',
+          'midi-program': '1',
+          'volume': '100',
+          'midi-channel': '10',
+          '$id': 'P1-X1'
+        },
+        'pitchMapping': {
+          'uE/4': {
+            'normal': '36',
+            'default': 'normal'
+          }
+        },
+        '$id': 'P1'
+      });
+
+      assert.deepEqual(scorePart[1], {
+        'part-name': 'Snare',
+        'part-abbreviation': '',
+        'score-instrument': {
+          'instrument-name': 'Snare',
+          '$id': 'P2-X1'
+        },
+        'midi-instrument': {
+          'midi-unpitched': '39',
+          'midi-program': '1',
+          'volume': '100',
+          'midi-channel': '10',
+          '$id': 'P2-X1'
+        },
+        'pitchMapping': {
+          'uE/4': {
+            'normal': '39',
+            'default': 'normal'
+          }
+        },
+        '$id': 'P2'
+      });
+
+      assert.deepEqual(scorePart[2], {
+        'part-name': 'Hi-Hat',
+        'part-abbreviation': '',
+        'score-instrument': {
+          'instrument-name': 'Hi-Hat',
+          '$id': 'P3-X1'
+        },
+        'midi-instrument': {
+          'midi-unpitched': '43',
+          'midi-program': '1',
+          'volume': '100',
+          'midi-channel': '10',
+          '$id': 'P3-X1'
+        },
+        'pitchMapping': {
+          'uE/4': {
+            'normal': '43',
+            'default': 'normal'
+          }
+        },
+        '$id': 'P3'
+      });
     });
   });
 });
